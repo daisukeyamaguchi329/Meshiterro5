82d01d2ce73dfeb708cd07c7a8ba0d69e3df7ac7
daisukeyamaguchi329 <ec2-user@ip-172-31-20-93.ec2.internal>
2021-08-25 13:59:50 +0000

    Third Commit

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'fc653661bf72a885d41c93756cd2c617e7fc368fe1d6070fc17d3632ea7eb0ce2c63e477d1ee5cbf9920d76018c74ba3040ee3e7017b329d31085bc332e651dc'
